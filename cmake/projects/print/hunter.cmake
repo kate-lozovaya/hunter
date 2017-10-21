696aa73c28dc026476a68bb431ce29c051ba1e56
kate-lozovaya <katekalozovaya@gmail.com>
2017-10-21 00:23:27 +0000

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/kate-lozovaya/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    c72a7234e2c2cc28369e4210a2d5df99a16695f6
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
